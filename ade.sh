bcee5aa1c2fddc0e77fa0770f900b1caec96b22c
marcomala46 <marcomala46@gmail.com>
2022-08-30 09:58:24 +0200

    condacolab

--- ade.sh
+++ ade.sh
@@ -1,31 +1,3 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 20
-#SBATCH --time=1:00:00
-#SBATCH --mem=150G
-#SBATCH -p cpu_bioscienze
-#SBATCH --mail-user=marco.malatesta@unipr.it
-#SBATCH --mail-type=BEGIN,END,FAIL
-
-
-while getopts l:d: flag
-do
-    case "${flag}" in
-        l) ligand=${OPTARG};;
-        d) directory=${OPTARG};;
-    esac
-done
-# cd $directory
-
-for file in ${directory}/*.trg;
-do
-   # wait here if the number of jobs is 3 (or more)
-   while (( (( $(jobs -p | wc -l) )) >= 20)) 
-   do 
-      sleep 5      # check again after 5 seconds
-   done
-
-total_ade.py $file $ligand &
-done
-wait
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:4b460a16e71d9ea1212d3de45f40de0f835445d02ed705dbc742a3bae0ffa5ff
+size 570
---
+++ condacolab-0.1-Linux-x86_64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df5aee54007a6103df19c27aa6e17c7d8ffd230a578726b80fdc3ab318487e7e
+size 624113583
